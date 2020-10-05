1604c99c203196e2752343fb6d67ca05a20ed797
Chris Kalama <chriskalama96@gmail.com>
2020-10-04 23:49:41 -0500

    added final components of what needed to be done in script. Attempted to write logic for each indiividual row's color based on time.

--- script.js
+++ script.js
@@ -3,8 +3,21 @@ var date = document.getElementById("currentDay");
 date = moment().format('LLLL');
     //document.getElementById('currentDay').innerHTML = "Today is " + date;
     $('#currentDay').html("Today is " + date)
-//Setting up If Date is
 
+//Setting up If Time is equal to date the Color will change. How can I link the Moment to Date? 
+var row = document.getElementsByClassName("row")
+if (date == row) {
+    $("row").function() {
+        $("row").css(".present")
+    }
+
+}
+else $("row").function() {
+    $("row").css(".future")
+}
+
+
+    
     
 
 //Setting up Saving an item to Local Storage
@@ -17,7 +30,8 @@ $(document).ready(function() {
     })
 });
 
-//Making sure Text will stay on screen when page is refreshed.
+//Making sure Text will stay on screen when page is refreshed. How do I grab from LocalStorage???
+
 //document.getElementsByClassName("user-text").innerHTML = localStorage.getItem("timeSave");
 //$('.user-text').html(localStorage.timeSave);
 //localStorage.getItem('timeSave');
