e7085350fa09f5506e0b473ee62f0ab90cd7de37
Chris Kalama <chriskalama96@gmail.com>
2020-10-04 22:48:20 -0500

    Added moment.js logic to display the date

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
+//Setting up Date 
+var date = document.getElementById("currentDay");
+date = moment().format('LLLL');
+    
+document.getElementById('currentDay').innerHTML = "Today is " + date;
+    
+
+//Setting up Saving an item to Local Storage
 $(document).ready(function() {
     $(".saveBtn").on("click", function(){
         var timeSave = $(this).parent().attr("id");
         var textSave= $(this).siblings(".user-text").val();
         localStorage.setItem(timeSave, textSave);
+        //localStorage.getItem('timeSave');
     })
 });
 
+//Making sure Text will stay on screen when page is refreshed.
+//document.getElementsByClassName("user-text").innerHTML = localStorage.getItem("timeSave");
+//$('.user-text').html(localStorage.timeSave);
+//localStorage.getItem('timeSave');
 
